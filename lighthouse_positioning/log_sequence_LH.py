bf37e97bc9bd87679eee7f3e81cee780d6899ba2
Jessica Tingley <jtingley2003@gmail.com>
2024-07-15 12:02:08 -0400

    Change wave sequence to use .go_to Add safety to ensure correct deck is attached

--- lighthouse_positioning/log_sequence_LH.py
+++ lighthouse_positioning/log_sequence_LH.py
@@ -17,7 +17,7 @@ URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E70A')
 logging.basicConfig(level=logging.ERROR)
 positions = []
 
-# deck_attached_event = Event()
+deck_attached_event = Event()
 
 
 def square_move_sequence(scf):
@@ -39,26 +39,30 @@ def square_move_sequence(scf):
 
 def up_down_move_sequence(scf):
     # Moves drones in a wave-like formation
-    with PositionHlCommander(scf, controller=PositionHlCommander.CONTROLLER_PID, default_height=0.5) as pc:
-        time.sleep(5)
-        for i in range(2):
-            pc.move_distance(distance_x_m=0, distance_y_m=0.3, distance_z_m=0.5)
-            time.sleep(1)
-            pc.move_distance(distance_x_m=0, distance_y_m=0.3, distance_z_m=-0.5)
-            time.sleep(1)
+    commander = scf.cf.high_level_commander
+    commander.takeoff(absolute_height_m=0.5, duration_s=1.5)
+    time.sleep(5)
+    for i in range(2):
+        commander.go_to(x=0, y=0.3, z=0.5, yaw=0, duration_s=1)
+        time.sleep(1)
+        commander.go_to(x=0, y=0.3, z=-0.5, yaw=0, duration_s=1)
+        time.sleep(1)
 
-        time.sleep(2)
-        pc.move_distance(distance_x_m=-0.5, distance_y_m=0, distance_z_m=0)
-        time.sleep(2)
+    time.sleep(2)
+    commander.go_to(x=-0.5, y=0, z=0, yaw=0, duration_s=2)
+    time.sleep(2)
 
-        for i in range(2):
-            pc.move_distance(distance_x_m=0, distance_y_m=-0.3, distance_z_m=0.5)
-            time.sleep(1)
-            pc.move_distance(distance_x_m=0, distance_y_m=-0.3, distance_z_m=-0.5)
-            time.sleep(1)
+    for i in range(2):
+        commander.go_to(x=0, y=-0.3, z=0.5, yaw=0, duration_s=1)
+        time.sleep(1)
+        commander.go_to(x=0, y=-0.3, z=-0.5, yaw=0, duration_s=1)
+        time.sleep(1)
 
-        time.sleep(2)
-        pc.move_distance(distance_x_m=-0.5, distance_y_m=0, distance_z_m=0)
+    time.sleep(2)
+    commander.go_to(x=0.5, y=0, z=0, yaw=0, duration_s=2)
+    time.sleep(2)
+
+    commander.land(absolute_height_m=0.0, duration_s=1.5)
 
 
 def log_pos_callback(timestamp, data, logconf):
@@ -67,14 +71,14 @@ def log_pos_callback(timestamp, data, logconf):
     positions.append([data['stateEstimate.x'], data['stateEstimate.y'], data['stateEstimate.z']])
 
 
-# def param_deck_flow(_, value_str):
-#     value = int(value_str)
-#     print(value)
-#     if value:
-#         deck_attached_event.set()
-#         print('Deck is attached!')
-#     else:
-#         print('Deck is NOT attached!')
+def param_deck(_, value_str):
+    value = int(value_str)
+    print(value)
+    if value:
+        deck_attached_event.set()
+        print('Deck is attached!')
+    else:
+        print('Deck is NOT attached!')
 
 
 if __name__ == '__main__':
@@ -83,10 +87,10 @@ if __name__ == '__main__':
     cflib.crtp.init_drivers()
 
     with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf:
-
-        # TO DO: safety to ensure necessary deck is attached
-        # scf.cf.param.add_update_callback(group='deck', name='bcFlow2',
-        #                                  cb=param_deck_flow)
+        # 1 = PID, 2 = Mellinger
+        scf.cf.param_set_value('stabilizer.controller', 1)
+        scf.cf.param.add_update_callback(group='deck', name='bcLighthouse4',
+                                         cb=param_deck)
         time.sleep(1)
 
         logconf = LogConfig(name='Position', period_in_ms=10)
@@ -96,9 +100,9 @@ if __name__ == '__main__':
         scf.cf.log.add_config(logconf)
         logconf.data_received_cb.add_callback(log_pos_callback)
 
-        # if not deck_attached_event.wait(timeout=5):
-        #     print('No flow deck detected!')
-        #     sys.exit(1)
+        if not deck_attached_event.wait(timeout=5):
+            print('No flow deck detected!')
+            sys.exit(1)
 
         logconf.start()
 
